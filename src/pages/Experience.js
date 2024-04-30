67fbec182a1e19e8abd8ec0dcc491e5323962f32
yqliukev <yqliukev@gmail.com>
2024-04-30 17:16:46 -0400

    change navbar location

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component" 
+import "react-vertical-timeline-component/style.min.css"
 
 function Experience() {
   return (
-    <div>
-      Experience
+    <div className="experience">
+      <VerticalTimeline>
+        <VerticalTimelineElement></VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
   )
 }
