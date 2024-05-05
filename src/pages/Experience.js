393c2e629406e5f2b395c227d87bd0ab3c2de8e4
Kevin Liu <kevin.liu@Kevins-Air-3.mshome.net>
2024-05-05 15:45:19 -0400

    start timeline

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,12 +4,24 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component" 
 import "react-vertical-timeline-component/style.min.css"
+import SchoolIcon from "@material-ui/icons/School"
 
 function Experience() {
   return (
     <div className="experience">
-      <VerticalTimeline>
-        <VerticalTimelineElement></VerticalTimelineElement>
+      <VerticalTimeline lineColor="black">
+        <VerticalTimelineElement 
+        className="vertical-timeline-element--education"
+        date="2014-2021"
+        iconStyle={{background: "darkgrey", colo: "black"}}
+        icon={<SchoolIcon/>}>
+          <h3 className="vertical-timeline-element-title">
+            University of Toronto Schools, Toronto Canada
+          </h3>
+          <p>
+            
+          </p>
+        </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
   )
